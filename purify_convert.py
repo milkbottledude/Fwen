458c9f375821a995442f64eb65c822a06ea9ccc3
Yu Zen <cheahyuzen@gmail.com>
2026-02-06 11:04:17 +0800

    combining low epoch training on mix of goggins in-out convo and monologue + system prompt yields best combi of personality and speech coherence, but coherence is still lacking (see evidence/gogginsTemu_mix_2ep_longSysPrompt

--- purify_convert.py
+++ purify_convert.py
@@ -3,12 +3,16 @@ import json
 def purify(filename, folder='subtitles'):
     with open(f'{folder}/{filename}.txt', 'r') as f:
         text = f.read()
+    for dirt in ['[music]', '>>']:
+        text = text.replace(dirt, '')
+    text = text.replace('[Â\xa0__Â\xa0]', 'damn')
     text = text.replace('\n', ' ')
     text = text.replace('"', "'")
     with open(f'{folder}/{filename}_clean.txt', 'w') as f:
         f.write(text)
 
-purify('goggins2')
+# for i in range(3, 7):
+#     purify(f'goggins{i}')
 
 def convert(filename, inputName, outputName):
     with open(f'subtitles/{filename}_clean.txt', 'r') as f:
@@ -25,7 +29,7 @@ def convert(filename, inputName, outputName):
     with open(f'jsonData/{filename}_JSON.json', 'w') as f:
         json.dump(toJSON, f, indent=4)
 
-convert('goggins2', 'Tom', 'David')
+# convert('goggins2', 'Tom', 'David')
 
 def combineJSON(name, num):
     combined = []
@@ -36,5 +40,50 @@ def combineJSON(name, num):
     with open(f'jsonData/{name}_JSON.json', 'w') as f:
         json.dump(combined, f, indent=4)
 
-combineJSON('goggins', 2)
+# combineJSON('goggins', 2)
         
+def monologue1(name):
+    try:
+        with open(f'jsonData/{name}_mono.json', 'r') as f:
+            just_me = json.load(f)
+    except FileNotFoundError:
+        just_me = []    
+    with open(f'jsonData/{name}_JSON.json', 'r') as f:
+        convo = json.load(f)
+        for pair in convo:
+            mono_dict = {
+                'input': "",
+                'output': pair['output']
+            }
+            just_me.append(mono_dict)
+    with open(f'jsonData/{name}_mono.json', 'w') as f:
+        json.dump(just_me, f, indent=4)
+
+# monologue('goggins')
+
+def monologue2(name, toMain=False): # straight from clean txt to json
+    if toMain:
+        with open(f'jsonData/{name[:-1]}_mono.json', 'r') as f:
+            just_me = json.load(f)
+    else:
+        just_me = []    
+    with open(f'subtitles/{name}_clean.txt', 'r') as f:
+        text = f.read()
+    textArr = text.split('. ')
+    for speech in textArr:
+        speech = speech.replace('[Â\xa0__Â\xa0]', 'damn')
+        mono_dict = {
+            'input': '',
+            'output': speech
+        }
+        just_me.append(mono_dict)
+    if toMain:
+        with open(f'jsonData/{name[:-1]}_mono.json', 'w') as f:
+            json.dump(just_me, f, indent=4)  
+    else:        
+        with open(f'jsonData/{name}_mono.json', 'w') as f:
+            json.dump(just_me, f, indent=4)    
+
+for i in range(3, 7):
+    monologue2(f'goggins{i}', toMain=True) 
+
